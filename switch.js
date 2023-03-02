1264354f8300546a57c59f4043a7ff42fd24f7b2
jonathan gober <Gober.jonathan@gmail.com>
2023-03-01 22:43:21 -0500

    test

--- switch.js
+++ switch.js
@@ -1,15 +1,19 @@
-
-switch (process.env) {
-    case process.env.QA:
+let env
+switch (process.env.NODE_ENV) {
+    case 'qa':
+        env = 'qa';
         console.log('running in the QA ENV');
         break;
-    case process.env.DEV:
+    case 'dev':
+        env = 'dev';
         console.log('running in the dev ENV');
         break;
-    case process.env.release:
+    case 'release':
+        env = 'release';
         console.log('running in the release ENV');
         break;
     default:
+        env = 'prod';
         console.log('running in the main branch');
         break;
 }
